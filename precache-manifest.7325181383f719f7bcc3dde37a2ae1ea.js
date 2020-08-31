bd02932921d4ee04bc7b2b609b65c13f30cf6c98
samzing777 <samuelopeyemi25@gmail.com>
2020-08-31 15:13:09 +0100

    Updates

---
+++ precache-manifest.7325181383f719f7bcc3dde37a2ae1ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a63ee3d4b67bae8bd24ac58fdf05ec",
+    "url": "/mytodo/index.html"
+  },
+  {
+    "revision": "147d78b819810aeb44ad",
+    "url": "/mytodo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ad01f13a58cb4c8db1e2",
+    "url": "/mytodo/static/css/main.e5ed8799.chunk.css"
+  },
+  {
+    "revision": "147d78b819810aeb44ad",
+    "url": "/mytodo/static/js/2.fbd063eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytodo/static/js/2.fbd063eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad01f13a58cb4c8db1e2",
+    "url": "/mytodo/static/js/main.01a1d80d.chunk.js"
+  },
+  {
+    "revision": "74732707ebaf8f5ddd37",
+    "url": "/mytodo/static/js/runtime-main.ae44bb6b.js"
+  }
+]);
